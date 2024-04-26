ddcbadbe3419da1c56fada9bcec0f979197d6fbf
Hakan Bayazıt Habeş <hakanbayazithabes@gmail.com>
2024-04-26 08:50:35 +0300

    Added : ApiException and ExceptionMiddleware classes to handle API exceptions and errors

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,4 @@
+using API.Middleware;
 using Core.Data;
 using Core.Interfaces;
 using Infrastructure.Data;
@@ -24,7 +25,10 @@ builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
+
 app.UseStatusCodePagesWithReExecute("/errors/{0}");
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
