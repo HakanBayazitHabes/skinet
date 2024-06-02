4ee4093bbe51b0abde1e81d165622107a48d61ce
Hakan Bayazıt Habeş <hakanbayazithabes@gmail.com>
2024-06-02 06:13:04 +0300

    Added : pagination functionality to product listing page

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Product } from './models/product';
+import { Pagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
@@ -8,17 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'Skinet';
-  products: any[] = [];
+  products: Product[] = [];
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/products?pagesize=50').subscribe({
-      next: (response: any) => (this.products = response.data),
-      error: (error) => console.log(error),
-      complete: () => {
-        console.log('completed');
-      },
-    });
+    this.http
+      .get<Pagination<Product[]>>(
+        'https://localhost:5001/api/products?pagesize=50'
+      )
+      .subscribe({
+        next: (response) => (this.products = response.data),
+        error: (error) => console.log(error),
+        complete: () => {
+          console.log('completed');
+        },
+      });
   }
 }
