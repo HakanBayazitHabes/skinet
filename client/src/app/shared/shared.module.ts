feb1b299f4472aa52dc12bfdcb4d36114ef4fb7f
Hakan Bayazıt Habeş <hakanbayazithabes@gmail.com>
2024-06-10 01:34:05 +0300

    Added : the home page content

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
 import { PaginHeaderComponent } from './pagin-header/pagin-header.component';
 import { PagerComponent } from './pager/pager.component';
+import { CarouselModule } from 'ngx-bootstrap/carousel'
 
 @NgModule({
   declarations: [PaginHeaderComponent, PagerComponent],
-  imports: [CommonModule, PaginationModule.forRoot()],
-  exports: [PaginationModule, PaginHeaderComponent, PagerComponent],
+  imports: [CommonModule, PaginationModule.forRoot(), CarouselModule.forRoot()],
+  exports: [PaginationModule, PaginHeaderComponent, PagerComponent, CarouselModule],
 })
-export class SharedModule {}
+export class SharedModule { }
