8bcae881ed15d798b443c7752e75bf5641c645d1
Hakan Bayazıt Habeş <hakanbayazithabes@gmail.com>
2024-06-13 14:07:45 +0300

    Refactoring basket service to handle item addition and update

--- client/src/app/basket/basket.service.ts
+++ client/src/app/basket/basket.service.ts
@@ -39,16 +39,19 @@ export class BasketService {
     const itemToAdd = this.mapProductItemToBasketItem(item);
     const basket = this.getCurrentBasketValue() ?? this.createBasket();
     basket.items = this.addOrUpdateItem(basket.items, itemToAdd, quantity);
+    this.setBasket(basket);
   }
 
   private addOrUpdateItem(items: BasketItem[], itemToAdd: BasketItem, quantity: number): BasketItem[] {
+    if (!items) {
+      items = [];
+    }
     const item = items.find(x => x.id === itemToAdd.id);
     if (item) item.quantity += quantity;
     else {
       itemToAdd.quantity = quantity;
       items.push(itemToAdd);
     }
-
     return items;
   }
 
