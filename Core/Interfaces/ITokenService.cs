c24f4559c664567a5aea4f5077f98ddfe9da08d5
Hakan Bayazıt Habeş <hakanbayazithabes@gmail.com>
2024-07-08 13:12:40 +0300

    Added : TokenService implementation are added to provide token generation functionality

---
+++ Core/Interfaces/ITokenService.cs
@@ -0,0 +1,8 @@
+﻿using Core.Entities.Identity;
+
+namespace Core;
+
+public interface ITokenService
+{
+    string CreateToken(AppUser user);
+}
---
+++ Infrastructure/Data/TokenService.cs
@@ -0,0 +1,44 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Core;
+using Core.Entities.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Infrastructure;
+
+public class TokenService : ITokenService
+{
+    public readonly IConfiguration _config;
+    private readonly SymmetricSecurityKey _key;
+    public TokenService(IConfiguration config)
+    {
+        _config = config;
+        _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Token:Key"]));
+    }
+
+    public string CreateToken(AppUser user)
+    {
+        var claim = new List<Claim>{
+            new(ClaimTypes.Email, user.Email),
+            new(ClaimTypes.GivenName, user.DisplayName)
+        };
+
+        var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(claim),
+            Expires = DateTime.Now.AddDays(7),
+            SigningCredentials = creds,
+            Issuer = _config["Token:Issuer"]
+        };
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+
+        return tokenHandler.WriteToken(token);
+    }
+}
