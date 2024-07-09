f7fc9b4fc394ebf69f8cbe0aa67635dfd5ee3b26
Hakan Bayazıt Habeş <hakanbayazithabes@gmail.com>
2024-07-09 12:30:31 +0300

    Added : CustomerBasketDto for managing customer baskets and swagger config for identity

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -83,6 +83,12 @@ namespace API.Controllers
         [HttpPost("register")]
         public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
+
+            if (CheckEmailExistsAsync(registerDto.Email).Result.Value)
+            {
+                return new BadRequestObjectResult(new ApiValidationErrorResponse { Errors = ["Email address is in use"] });
+            }
+
             var user = new AppUser
             {
                 DisplayName = registerDto.DisplayName,
@@ -101,6 +107,6 @@ namespace API.Controllers
                 Email = user.Email
             };
         }
-    
+
     }
 }
--- API/Controllers/BasketController.cs
+++ API/Controllers/BasketController.cs
@@ -1,12 +1,15 @@
+using API.Dtos;
+using AutoMapper;
 using Core.Entities;
 using Core.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
 
-public class BasketController(IBasketRepository basketRepository) : BaseApiController
+public class BasketController(IBasketRepository basketRepository, IMapper mapper) : BaseApiController
 {
     private readonly IBasketRepository _basketRepository = basketRepository;
+    private readonly IMapper _mapper = mapper;
 
     [HttpGet]
     public async Task<ActionResult<CustomerBasket>> GetBasketById(string id)
@@ -17,9 +20,11 @@ public class BasketController(IBasketRepository basketRepository) : BaseApiContr
     }
 
     [HttpPost]
-    public async Task<ActionResult<CustomerBasket>> UpdateBasket(CustomerBasket basket)
+    public async Task<ActionResult<CustomerBasket>> UpdateBasket(CustomerBasketDto basket)
     {
-        var updatedBasket = await _basketRepository.UpdateBasketAsync(basket);
+        var customerBasket = _mapper.Map<CustomerBasketDto, CustomerBasket>(basket);
+        
+        var updatedBasket = await _basketRepository.UpdateBasketAsync(customerBasket);
 
         return Ok(updatedBasket);
     }
