f73895fc91ef6afb040f2678f2862f8d9e5ad4e7
Hakan Bayazıt Habeş <hakanbayazithabes@gmail.com>
2024-07-30 01:32:00 +0300

    Added : text-input component for form input fields

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -7,9 +7,10 @@ import { CarouselModule } from 'ngx-bootstrap/carousel';
 import { OrderTotalsComponent } from './order-totals/order-totals.component'
 import { ReactiveFormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TextInputComponent } from './components/text-input/text-input.component';
 
 @NgModule({
-  declarations: [PaginHeaderComponent, PagerComponent, OrderTotalsComponent],
+  declarations: [PaginHeaderComponent, PagerComponent, OrderTotalsComponent, TextInputComponent],
   imports: [CommonModule, PaginationModule.forRoot(), CarouselModule.forRoot(), ReactiveFormsModule, BsDropdownModule.forRoot()],
   exports: [PaginationModule, PaginHeaderComponent, PagerComponent, CarouselModule, OrderTotalsComponent, ReactiveFormsModule, BsDropdownModule],
 })
